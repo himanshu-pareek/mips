7494d39c4bdaa442c42847bea3bec4effbe0bcc9
himanshu <hp746123@gmail.com>
2017-09-01 17:44:40 +0530

    Modifies (partially) fib_s.asm file

--- fib_s.asm
+++ fib_s.asm
@@ -7,4 +7,12 @@
 
 .text
 
-
+fib:
+	subu	$sp, $sp, 32		# frame size = 32, just because...
+	sw	$ra, 28($sp)		# preserve the Return Address.
+	sw	$fp, 24($sp)		# preserve the Frame Pointer.
+	sw	$s0, 20($sp)		# preserve $s0.
+	sw	$s1, 16($sp)		# preserve $s1.
+	sw	$s2, 12($sp)		# preserve $s2.
+	addu	$fp, $sp, 32		# move Frame Pointer to base of frame.
+	
